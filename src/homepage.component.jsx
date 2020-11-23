9d1d4bf18126e3b60aff836758645beaf679ebab
rodrigomarigo <rodrigomarigo@gmail.com>
2020-11-23 09:12:13 -0300

    Criacao do app com create react app e adicao de styles simples para homepage

---
+++ src/homepage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const HomePage = () => (
+    <div className = 'homepage'>
+        <div className = 'pasta-menu'>
+            <div className = 'item-menu'>
+                <div className = 'conteudo'>
+                    <h1 className = 'titulo'>SALGADOS</h1>
+                    <span className = 'subtitulo'>COMPRAR AGORA!</span>
+                </div>
+            </div>
+            <div className = 'item-menu'>
+                <div className = 'conteudo'>
+                    <h1 className = 'titulo'>DOCES</h1>
+                    <span className = 'subtitulo'>COMPRAR AGORA!</span>
+                </div>
+            </div>
+            <div className = 'item-menu'>
+                <div className = 'conteudo'>
+                    <h1 className = 'titulo'>BEBIDAS</h1>
+                    <span className = 'subtitulo'>COMPRAR AGORA!</span>
+                </div>
+            </div>
+            <div className = 'item-menu'>
+                <div className = 'conteudo'>
+                    <h1 className = 'titulo'>CONGELADOS</h1>
+                    <span className = 'subtitulo'>COMPRAR AGORA!</span>
+                </div>
+            </div>
+            <div className = 'item-menu'>
+                <div className = 'conteudo'>
+                    <h1 className = 'titulo'>PREPARE EM CASA</h1>
+                    <span className = 'subtitulo'>COMPRAR AGORA!</span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
