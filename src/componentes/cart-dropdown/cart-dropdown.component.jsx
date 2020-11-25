01aec6d7058a38e6e19ee4dbcf7eef14fe502ac9
rodrigomarigo <rodrigomarigo@gmail.com>
2020-11-25 16:39:12 -0300

    Atualizando cart-dropdown para funcionar com selectors, otimizando o numero de renderização na navegação

--- src/componentes/cart-dropdown/cart-dropdown.component.jsx
+++ src/componentes/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import './cart-dropdown.style.scss';
 
 import CustomButton from '../custom-button/custom-button.component.jsx';
 import CartItem from '../cart-item/cart-item.component.jsx';
+import { selectCartItems } from '../../redux/cart/cart.selectors'
 
 const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
   );
   
-  const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+  const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
   });
   
   export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,19 @@
+ //reduzir a quantidade de vezes que um componente é renderizado (somente com mudanças) 
+import { createSelector } from 'reselect';
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
