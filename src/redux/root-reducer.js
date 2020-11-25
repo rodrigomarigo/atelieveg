65109c6b800b750d9b97d958a28c91048fb69513
rodrigomarigo <rodrigomarigo@gmail.com>
2020-11-25 20:22:39 -0300

    Adicionado persist para manter a informação da sacola caso atualize a pagina

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,9 +1,20 @@
 import { combineReducers } from 'redux';
 import userReducer from './user/user.reducer.js';
 import cartReducer from './cart/cart.reducer.js';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-// controla todos os outros reducers
-export default combineReducers({
+// Manter a informação mesmo ao atualizar a página
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist:['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 })
+
+// controla todos os outros reducers
+export default persistReducer(persistConfig, rootReducer);
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,11 +2,13 @@ import { createStore, applyMiddleware } from 'redux';
 
 // para enxergar o reducer se precisar
 import logger from 'redux-logger';
-
+import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
